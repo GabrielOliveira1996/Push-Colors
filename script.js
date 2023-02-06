731a29b5f076029779dc23d96c15d3b196b403ae
Gabriel <gab-oliveira96@hotmail.com>
2023-02-06 13:33:14 -0300

    Game Genius | Iniciado desenvolvimento de código em que verifica se a escolha de cor do usuário esta correta.

--- script.js
+++ script.js
@@ -13,6 +13,7 @@ createApp({
             vermelho: 'preto',
             azul: 'preto',
             amarelo: 'preto',
+            contadorSequeniaDeCores: 0,
         }
     },
     methods: {
@@ -21,6 +22,8 @@ createApp({
 
             let numero = Math.floor(Math.random() * 4);
             let contador = 0;
+            this.sequenciaJogador.length = 0;
+            this.contadorSequeniaDeCores = 0;
             this.sequencia.push(this.cores[numero]);
             let intervalo = setInterval(()=>{
                 //contador = this.iniciarCores();
@@ -77,41 +80,133 @@ createApp({
 
         },
         compararSequencia(e){
-            
+ 
+            console.log('contador no final do click'+this.contadorSequeniaDeCores);
             //adicionar funcionalidade para apagar div
-            if(e.target.id == 'verdeId'){
-
-                this.sequenciaJogador += 'verde';
-                setTimeout(()=>{
-                    this.verde = 'verde';
-                    console.log('verde');
-                }, 1000);
+            if(this.sequencia.length > 0){
                 
-            }else if(e.target.id == 'vermelhoId'){
+                if(e.target.id == 'verdeId'){
 
-                this.sequenciaJogador += 'vermelho';
-                setTimeout(()=>{
-                    this.vermelho = 'vermelho';
-                    console.log('vermelho');
-                }, 1000);
-                
-            }else if(e.target.id == 'azulId'){
+                    this.sequenciaJogador.push(this.cores[0]);
+                    setTimeout(()=>{
+                        this.verde = 'verde';
+                    }, 0);
 
-                this.sequenciaJogador += 'azul';
-                setTimeout(()=>{
-                    this.azul = 'azul';
-                    console.log('azul');
-                }, 1000);
-                
-            }else if(e.target.id == 'amareloId'){
+                    setTimeout(()=>{
+                        this.verde = 'preto';
+                    }, 1000);
+                    
+                    if('verde' === this.sequencia[this.contadorSequeniaDeCores]){
+                        //emitir som da div 
+                        console.log('acertou');
+                        console.log('sequencia: '+this.sequencia[this.contadorSequeniaDeCores]);
+                        console.log('sequencia jogador: '+'verde');
+                        this.contadorSequeniaDeCores += 1;
+                        // testar se as entradas são iguais
+                    }else{
+                        console.log('errou');
+                        this.sequencia.length = 0;
+                        this.sequenciaJogador.length = 0;
+                        this.contadorSequeniaDeCores = 0;
+                        console.log(this.sequencia);
+                        console.log(this.sequenciaJogador);
+                    }
 
-                this.sequenciaJogador += 'amarelo';
-                setTimeout(()=>{
-                    this.amarelo = 'amarelo';
-                    console.log('amarelo'); 
-                }, 1000);
+                    
+                }else if(e.target.id == 'vermelhoId'){
 
+                    this.sequenciaJogador.push(this.cores[1]);
+                    // testar se as entradas são iguais
+                    console.log(this.sequencia);
+                    console.log(this.sequenciaJogador);
+                    setTimeout(()=>{
+                        this.vermelho = 'vermelho';
+                    }, 0);
+
+                    setTimeout(()=>{
+                        this.vermelho = 'preto';
+                    }, 1000);
+
+                    if('vermelho' === this.sequencia[this.contadorSequeniaDeCores]){
+                        //emitir som da div 
+                        console.log('acertou');
+                        console.log('sequencia: '+this.sequencia[this.contadorSequeniaDeCores]);
+                        console.log('sequencia jogador: '+'vermelho');
+                        this.contadorSequeniaDeCores += 1;
+                        // testar se as entradas são iguais
+                    }else{
+                        console.log('errou');
+                        this.sequencia.length = 0;
+                        this.sequenciaJogador.length = 0;
+                        this.contadorSequeniaDeCores = 0;
+                        console.log(this.sequencia);
+                        console.log(this.sequenciaJogador);
+                    }
+                    
+                }else if(e.target.id == 'azulId'){
+
+                    this.sequenciaJogador.push(this.cores[2]);
+                    // testar se as entradas são iguais
+                    console.log(this.sequencia);
+                    console.log(this.sequenciaJogador);
+                    setTimeout(()=>{
+                        this.azul = 'azul';
+                    }, 0);
+
+                    setTimeout(()=>{
+                        this.azul = 'preto';
+                    }, 1000);
+
+                    if('azul' === this.sequencia[this.contadorSequeniaDeCores]){
+                        //emitir som da div 
+                        console.log('acertou');
+                        console.log('sequencia: '+this.sequencia[this.contadorSequeniaDeCores]);
+                        console.log('sequencia jogador: '+'azul');
+                        this.contadorSequeniaDeCores += 1;
+                        // testar se as entradas são iguais
+                    }else{
+                        console.log('errou');
+                        this.sequencia.length = 0;
+                        this.sequenciaJogador.length = 0;
+                        this.contadorSequeniaDeCores = 0;
+                        console.log(this.sequencia);
+                        console.log(this.sequenciaJogador);
+                    }
+                    
+                }else if(e.target.id == 'amareloId'){
+
+                    this.sequenciaJogador.push(this.cores[3]);
+                    // testar se as entradas são iguais
+                    console.log(this.sequencia);
+                    console.log(this.sequenciaJogador);
+                    setTimeout(()=>{
+                        this.amarelo = 'amarelo';
+                    }, 0);
+
+                    setTimeout(()=>{
+                        this.amarelo = 'preto';
+                    }, 1000);
+
+                    if('amarelo' === this.sequencia[this.contadorSequeniaDeCores]){
+                        //emitir som da div 
+                        console.log('acertou');
+                        console.log('sequencia: '+this.sequencia[this.contadorSequeniaDeCores]);
+                        console.log('sequencia jogador: '+'amarelo');
+                        this.contadorSequeniaDeCores += 1;
+                        // testar se as entradas são iguais
+                        
+                    }else{
+                        console.log('errou');
+                        this.sequencia.length = 0;
+                        this.sequenciaJogador.length = 0;
+                        this.contadorSequeniaDeCores = 0;
+                        console.log(this.sequencia);
+                        console.log(this.sequenciaJogador);
+                    }
+
+                }
             }
+            
             
             
         }
